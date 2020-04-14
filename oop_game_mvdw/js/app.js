453f4ae6fa959c2863c1c11cbc71d33ffee1497a
Michelle <62473111+MichellevdWalt@users.noreply.github.com>
2020-04-14 17:09:32 +0200

    update

--- oop_game_mvdw/js/app.js
+++ oop_game_mvdw/js/app.js
@@ -4,6 +4,12 @@
 let newGame;
 let newPhrase;
 
+/**
+ * Function to create a new game from the Game class in Game.js. 
+ * Creates a new Phrase using the activePhrase property of the newGame. Class Phrase found in Phrase.js.
+ * Event listener on overlay button calls startGame() function of the newGame variable. Found in Game.js.
+ * Calls handleInteraction() of the newGame variable. Found in Game.js.
+ */
 function startButton(){
     const startBut = document.querySelector('#btn__reset');
     newGame = new Game();
